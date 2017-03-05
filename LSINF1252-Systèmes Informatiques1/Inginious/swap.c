0a144d09112d261b39d6a587c66e4a79547a087f
Twan0u <ant.lamb.al@gmail.com>
2017-03-05 10:18:28 +0100

    optimisation du code

--- LSINF1252-Systèmes Informatiques1/Inginious/swap.c
+++ LSINF1252-Systèmes Informatiques1/Inginious/swap.c
@@ -1,1 +1,9 @@
- 
+
+void swap(struct fract_t *a, struct fract_t *b) {
+	int tmp = a->num;// on optimise le code en utilisant qu'une seule variable 
+	a->num = b->num;
+	b->num = tmp;
+	tmp = a->denum;
+	a->denum = b->denum;
+	b->denum = tmp;
+}
